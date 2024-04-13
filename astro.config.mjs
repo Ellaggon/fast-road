09b2f5196ec1e528a0d87f0e68342f3183acb9ba
Ellioth Aguirre <ellaggon@gmail.com>
2024-04-12 22:07:21 -0400

    add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import auth from "auth-astro";
 import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
 import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
 
 
 // https://astro.build/config
 export default defineConfig({
   // ...
   output: "server",
+  adapter: cloudflare({ mode: "directory" }),
   integrations: [tailwind(), react(), auth()],
   adapter: node({
     mode: "standalone"
